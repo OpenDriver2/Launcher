9723cc7ba0fe8b3e202013bb1be5383798be1ae2
Ilya Shurumov <ishurumov@gmail.com>
2021-05-28 22:02:34 +0600

    - improved ISO9660 dumper and fix bugs - game language auto-detection - image checking

--- iso9660_dump.cpp
+++ iso9660_dump.cpp
@@ -18,14 +18,19 @@ typedef	unsigned long	u_long;
 
 #include "iso9660.h"
 #include <stdio.h>
+#include <string.h>
 #include <Core/Core.h>
 
 #if defined (__unix__)
-#include <sys/stat.h>
-#define _mkdir(str)				mkdir(str, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)
+#	include <sys/stat.h>
+#	define _mkdir(str)				mkdir(str, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)
+#	define PATH_SEPARATOR '/'
 #else
-#include <direct.h>
+#	include <direct.h>
+#	define PATH_SEPARATOR '\\'
 #endif
+
+#include <minmax.h>
 
 struct CDImage_t
 {
@@ -33,31 +38,35 @@ struct CDImage_t
 	int sectorSize; // CD_SECTOR_SIZE_MODE2
 };
 
-// dumps ISO 9660 compatible image file into target folder
-void DumpFiles_r(CDImage_t* im, Sector* sector, const char* curPath)
+typedef bool (*fileFunc_t)(CDImage_t* im, TOC* toc, const char* filename, void* userData);
+typedef bool (*folderFunc_t)(CDImage_t* im, TOC* toc, const char* foldername, void* userData);
+
+// walks through all files inside ISO 9660 filesystem
+bool WalkFiles_r(CDImage_t* im, const char* curPath, fileFunc_t fileFunc, folderFunc_t folderFunc = NULL, void* userData = NULL)
 {
-	char nameStr[256];
+	Sector sector;
+	char nameStr[512];
 	TOC* toc;
 	int tocLocalOffset;
 
-	toc = (TOC*)&sector->data[0];
+	// read TOC from sector
+	fread(&sector, sizeof(Sector), 1, im->fp);
+	toc = (TOC*)&sector.data[0];
 	tocLocalOffset = 0;
-
-	if(*curPath == '\\')
-		curPath++;
-
-	// create directory structure
-	_mkdir(curPath);
+	
+	// useful for mkdir
+	if(folderFunc)
+		folderFunc(im, toc, curPath, userData);
 
 	while (toc->tocEntryLength != 0)
 	{
-		char* itemNameStr = (char*)&sector->data[tocLocalOffset + sizeof(TOC)];
+		char* itemNameStr = (char*)&sector.data[tocLocalOffset + sizeof(TOC)];
 		
 		// skip . and ..
 		if (*itemNameStr == 0 || *itemNameStr == 1)
 		{
 			tocLocalOffset += toc->tocEntryLength;
-			toc = (TOC*)&sector->data[tocLocalOffset];
+			toc = (TOC*)&sector.data[tocLocalOffset];
 
 			continue;
 		}
@@ -65,87 +74,208 @@ void DumpFiles_r(CDImage_t* im, Sector* sector, const char* curPath)
 		if (toc->flags & 2) // is directory
 		{
 			// read a sector
-			Sector nsector;
-			sprintf(nameStr, "%s\\%s", curPath, itemNameStr);
-
 			fseek(im->fp, toc->sectorPosition[0] * im->sectorSize, SEEK_SET);
-			fread(&nsector, sizeof(Sector), 1, im->fp);
 
-			DumpFiles_r(im, &nsector, nameStr);
+			// append directory
+			sprintf(nameStr, "%s%c%s", curPath, PATH_SEPARATOR, itemNameStr);
+			
+			// enter directory and walk here
+			if(!WalkFiles_r(im, nameStr, fileFunc, folderFunc, userData))
+				return false;
 		}
 		else
 		{
-			Sector nsector;
-
-			int remainingBytes = toc->fileSize[0];
-
-			// seek to start
+			// seek to file start
 			fseek(im->fp, toc->sectorPosition[0] * im->sectorSize, SEEK_SET);
-			fread(&nsector, sizeof(Sector), 1, im->fp);
-
-			int interleaved = nsector.subHead[0] == 1;
-
-			// [A] don't extract form 2
-			if(!interleaved)
-			{
-				sprintf(nameStr, "%s\\%s", curPath, itemNameStr);
-
-				char* verStart = strchr(nameStr, ';');
-				if (verStart)
-					*verStart = '\0';
-
-				// save file
-				FILE* fp = fopen(nameStr, "wb");
-				if (fp)
-				{
-					do
-					{
-						fread(&nsector, sizeof(Sector), 1, im->fp);
-
-						if (!interleaved)
-						{
-							Sector* pSector = &nsector;
-							remainingBytes -= sizeof(pSector->data);
-
-							fwrite(pSector->data, 1, sizeof(pSector->data), fp);
-						}
-						else
-						{
-							// extract whole sectors?
-							// FIXME: wtf with file sizes?
-							AudioSector* pSector = (AudioSector*)&nsector;
-							remainingBytes -= sizeof(pSector->data);
-
-							fwrite(pSector, 1, sizeof(AudioSector), fp);
-						}
-
-						fread(&nsector, sizeof(Sector), 1, im->fp);
-
-					} while (remainingBytes > 0);
-				}
-
-				// XA flag: (nsector.subHead[2] & (1 << 5)) ? 2 : 1
-				// form 2 flag: nsector.subHead[0] & 1
-
-				/*printf("%s\\%s - form %d - %x %x %x %x %x %x %x %x\n", curPath, itemNameStr, (nsector.subHead[0] & 1) ? 2 : 1,
-					nsector.subHead[0],
-					nsector.subHead[1],
-					nsector.subHead[2],
-					nsector.subHead[3],
-					nsector.subHead[4],
-					nsector.subHead[5],
-					nsector.subHead[6],
-					nsector.subHead[7]);*/
-			}
+			
+			// append directory
+			sprintf(nameStr, "%s%c%s", curPath, PATH_SEPARATOR, itemNameStr);
+			
+			// exec callback
+			if(!fileFunc( im, toc, nameStr, userData ))
+				return false;
 		}
-
-		// next file
+		
 		tocLocalOffset += toc->tocEntryLength;
-		toc = (TOC*)&sector->data[tocLocalOffset];
+		toc = (TOC*)&sector.data[tocLocalOffset];
 	}
+	
+	return true;
 }
 
-bool DumpImageFile(const char* filename, const char* targetFolder)
+// callback function for creating folder
+bool CreateFolderFunc(CDImage_t* im, TOC* toc, const char* folderName, void* userData)
+{
+	if(*folderName == PATH_SEPARATOR)
+		folderName++;
+	
+	_mkdir(folderName);
+	return true;
+}
+
+// callback function for dumping a file
+bool DumpFileFunc(CDImage_t* im, TOC* toc, const char* filename, void* userData)
+{
+	Sector sector;
+	int remainingBytes, interleaved;
+	
+	// read first sector
+	fread(&sector, sizeof(Sector), 1, im->fp);
+
+	interleaved = sector.subHead[0] == 1;
+	remainingBytes = toc->fileSize[0];
+	
+	if(*filename == PATH_SEPARATOR)
+		filename++;
+	
+	// skip few files
+	// TODO: detect language here!!!!
+	if( strstr(filename, "SYSTEM.CNF") ||
+		strstr(filename, "SLES_") ||
+		strstr(filename, "SLUS_") ||
+		strstr(filename, "FMV.EXE") ||
+		strstr(filename, "FRONTEND.BIN") ||
+		strstr(filename, "LEAD.BIN") ||
+		strstr(filename, "PATH.BIN") ||
+		strstr(filename, "MCARD.BIN") ||
+		strstr(filename, "ATTRACT.40") )
+	{
+		return true;
+	}
+	
+	// don't extract form 2
+	if(!interleaved)
+	{
+		char* verStart = (char*)strchr(filename, ';');
+		if (verStart)
+			*verStart = '\0';
+
+		// save file
+		FILE* fp = fopen(filename, "wb");
+		if (fp)
+		{
+			do
+			{
+				if (!interleaved)
+				{
+					Sector* pSector = &sector;
+					
+					fwrite(pSector->data, 1, min(remainingBytes, sizeof(pSector->data)), fp);
+					
+					remainingBytes -= sizeof(pSector->data);
+				}
+				else
+				{
+					// extract whole sectors?
+					// FIXME: wtf with file sizes?
+					AudioSector* pSector = (AudioSector*)&sector;
+
+					fwrite(pSector, 1, min(remainingBytes, sizeof(AudioSector)), fp);
+					
+					remainingBytes -= sizeof(AudioSector); //sizeof(pSector->data);
+
+				}
+				
+				// read next sector
+				fread(&sector, sizeof(Sector), 1, im->fp);
+
+			} while (remainingBytes > 0);
+			
+			fclose(fp);
+		}
+
+		// XA flag: (sector.subHead[2] & (1 << 5)) ? 2 : 1
+		// form 2 flag: sector.subHead[0] & 1
+
+		/*printf("%s\\%s - form %d - %x %x %x %x %x %x %x %x\n", curPath, itemNameStr, (sector.subHead[0] & 1) ? 2 : 1,
+			sector.subHead[0],
+			sector.subHead[1],
+			sector.subHead[2],
+			sector.subHead[3],
+			sector.subHead[4],
+			sector.subHead[5],
+			sector.subHead[6],
+			sector.subHead[7]);*/
+	}
+	
+	return true;
+}
+
+// callback function for validating game files
+bool ValidateFileFunc(CDImage_t* im, TOC* toc, const char* filename, void* userData)
+{
+	const char* sub = strstr(filename, "FONT2.FNT");
+	return (sub == 0);
+}
+
+// callback function for detecting game language by executable file
+bool DetectGameLanguageFunc(CDImage_t* im, TOC* toc, const char* filename, void* userData)
+{
+	int* languageId = (int*)userData;
+	if(strstr(filename, "SLUS_"))
+	{
+		*languageId = 0;	// ENGLISH, US
+		return false;
+	}
+	else
+	{
+		const char* sub = strstr(filename, "SLES");
+		
+		if(sub)
+		{
+			sub = strchr(sub, '.');
+			if(sub)
+			{
+				switch(sub[2])
+				{
+					case '3':
+						*languageId = 0;	// ENGLISH, UK
+						break;
+					case '4':
+						*languageId = 3;	// FRENCH
+						break;
+					case '5':
+						*languageId = 2;	// GERMAN
+						break;
+					case '6':
+						*languageId = 1;	// ITALIAN
+						break;
+					case '7':
+						*languageId = 4;	// SPANISH
+						break;
+				}
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+int DetectDriver2Language(const char* filename)
+{
+	int languageId = -1;
+	CDImage_t im;
+	
+	im.sectorSize = CD_SECTOR_SIZE_MODE2;
+	im.fp = fopen(filename, "rb");
+	
+	if(!im.fp)
+		return false;
+	
+	// goto first sector and proceed
+	fseek(im.fp, CD_ROOT_DIRECTORY_SECTOR * im.sectorSize, SEEK_SET);
+	if(!WalkFiles_r(&im, "", DetectGameLanguageFunc, NULL, &languageId))
+	{
+		fclose(im.fp);
+		return languageId;
+	}
+	
+	fclose(im.fp);
+	return languageId;
+}
+
+// validates file
+bool CheckIsDriver2GameImage(const char* filename)
 {
 	CDImage_t im;
 	
@@ -155,12 +285,32 @@ bool DumpImageFile(const char* filename, const char* targetFolder)
 	if(!im.fp)
 		return false;
 	
-	// read first sector and proceed
-	Sector sector;
+	// goto first sector and proceed
 	fseek(im.fp, CD_ROOT_DIRECTORY_SECTOR * im.sectorSize, SEEK_SET);
-	fread(&sector, sizeof(Sector), 1, im.fp);
+	if(!WalkFiles_r(&im, "", ValidateFileFunc))
+	{
+		fclose(im.fp);
+		return true;
+	}
 	
-	DumpFiles_r(&im, &sector, "");
+	fclose(im.fp);
+	return false;
+}
+
+// dumps entire ISO-9660 filesystem to current working folder
+bool DumpImageFile(const char* filename)
+{
+	CDImage_t im;
+	
+	im.sectorSize = CD_SECTOR_SIZE_MODE2;
+	im.fp = fopen(filename, "rb");
+	
+	if(!im.fp)
+		return false;
+	
+	// goto first sector and proceed
+	fseek(im.fp, CD_ROOT_DIRECTORY_SECTOR * im.sectorSize, SEEK_SET);
+	WalkFiles_r(&im, "", DumpFileFunc, CreateFolderFunc);
 	
 	fclose(im.fp);
 	
--- iso9660_dump.h
+++ iso9660_dump.h
@@ -1,6 +1,8 @@
 #ifndef _RED2Launcher_iso9660_dump_h_
 #define _RED2Launcher_iso9660_dump_h_
 
-bool DumpImageFile(const char* filename, const char* targetFolder);
+int 	DetectDriver2Language(const char* filename);
+bool 	CheckIsDriver2GameImage(const char* filename);
+bool 	DumpImageFile(const char* filename);
 
 #endif
